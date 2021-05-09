e6c8a63c69258e5a31cb5a1acf152a3aea195caf
willmas-here <65773478+willmas-here@users.noreply.github.com>
2021-05-09 10:13:33 +0800

    Added testAuth Resolver

--- server/src/resolvers/hello.ts
+++ server/src/resolvers/hello.ts
@@ -1,9 +1,16 @@
-import { Query, Resolver } from "type-graphql";
+import { isAuth } from "../middlewares/isAuth";
+import { Query, Resolver, UseMiddleware } from "type-graphql";
 
 @Resolver()
 export class HelloResolver {
   @Query(() => String)
   hello() {
     return "hello world";
+  }
+
+  @UseMiddleware(isAuth)
+  @Query(() => String)
+  testAuth() {
+    return "authenticated";
   }
 }
